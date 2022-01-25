984e2a309f0f1a3e7984f5d9c507017333db8274
akbarhiday8 <akbar.nurhiday8@gmail.com>
2022-01-25 09:53:07 +0700

    first commit

---
+++ javascript.js
@@ -0,0 +1,51 @@
+//menu
+
+let tombolMenu = $(".tombol-menu");
+let menu = $("nav .menu ul");
+
+function klikMenu(){
+    tombolMenu.click(function(){
+        menu.toggle();
+    });
+    menu.click(function(){
+        menu.toggle();
+    });
+}
+
+$(document).ready(function(){
+    let width = $(window).width();
+    if(width < 990){
+        klikMenu();
+
+    }
+})
+
+//check lebar
+$(window).resize(function(){
+    let width = $(window).width();
+    if(width > 989){
+        menu.css("display","block");
+        //display:block
+    }else{
+        menu.css("display","none");
+    }
+    klikMenu();
+});
+
+//efek scroll
+$(document).ready(function(){
+    let scroll_pos = 0;
+    $(document).scroll(function(){
+        scroll_pos = $(this).scrollTop();
+        if(scroll_pos > 0){
+            $("nav").addClass("putih");
+            $("nav img.hitam").show();
+            $("nav img.putih").hide();
+        }else{
+            $("nav").removeClass("putih");
+            $("nav img.hitam").hide();
+            $("nav img.putih").show();
+        }
+    })
+
+});
